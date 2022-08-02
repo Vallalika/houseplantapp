07948d845f6015cea2f885f94b904f321581bf3a
Valerie Bayard <valerie.bayard@gmail.com>
2022-08-02 15:54:38 +0100

    modified text in /api route

--- server/src/main/java/com/codeclan/houseplantapp/server/controllers/ApiController.java
+++ server/src/main/java/com/codeclan/houseplantapp/server/controllers/ApiController.java
@@ -1,9 +1,7 @@
 package com.codeclan.houseplantapp.server.controllers;
 
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -12,13 +10,14 @@ public class ApiController {
 
     @GetMapping(value="/")
     public RedirectView redirectToApiRoute(RedirectAttributes attributes){
-        attributes.addAttribute("attribute", "/");
+        attributes.addAttribute("baseUrl", "/");
         return new RedirectView("api");
     }
 
     @GetMapping(value = "/api")
     public String apiRoutes () {
         return (
+                "Base URL for this API: /api <br > <br />" +
                 "Available routes for this API: <br /> <br />" +
                 "&nbsp; /gardens <br />" +
                 "&nbsp; /gardens/{id} <br /> <br />" +
